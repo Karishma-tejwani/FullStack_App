b689660087321213fe6c7ab8afa7b02720f12aae
Karishma-tejwani <karishmatejwani9@gmail.com>
2022-07-18 18:47:41 +0500

    client

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,42 +3,33 @@ import { NavLink } from "react-router-dom";
 
 function Add() {
 
-    const [val, setVal] = useState({
-        name:"",
-        email:"",
-        job:"",
-        phone:"",
-        address:"",
-    })
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [job, setJob] = useState();
+    const [phone, setPhone] = useState();
+    const [address, setAddress] = useState();
 
-    const setData = (e) =>{
-        console.log(e.target.value);
-        const {name, value} = e.target;
-        setVal((preval) => {
-            return{
-                ...preval, [name]:value
-            }
-        })
-    }
-
-    const addData = async(e) =>{
+       const addData = async(e) =>{
         e.preventDefault();
-        const {name, email, job, phone, address} = val;
+        
+        const val = {name, email, job, phone, address};
+        console.log(val);
+
         const res = await fetch("/add", {
             method: "POST",
             headers: {"Content-Type":"application/json"},
-            body: JSON.stringify({name, email, job, phone, address})
+            body: JSON.stringify(val)
         });
 
         const data = await res.json();
         console.log(data);
 
         if(res.status === 404 || !data){
-            alert("error");
-            console.log("err");
+            alert("Data not inserted!");
+            console.log("Data not inserted!");
         }else{
-            alert("data added");
-            console.log("data added");
+            alert("Data Inserted!");
+            console.log("Data Inserted!");
         }
     }
 
@@ -51,23 +42,23 @@ function Add() {
                         <label for="exampleInputName" className="form-label">Name</label>
                         <input type="text" className="form-control" id="exampleInputName" 
                             name="name"
-                            value={val.name}
-                            onChange={setData}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div className="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputEmail1" className="form-label">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" 
                             name="email"
-                            value={val.email}
-                            onChange={setData}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     {/* <div className="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" className="form-label">Password</label>
                         <input type="password" className="form-control" id="exampleInputPassword1" 
                             name="password"
-                            value={val.password}
+                            value={password}
                             onChange={setData}
                         />
                     </div> */}
@@ -75,24 +66,24 @@ function Add() {
                         <label for="exampleInputJob" className="form-label">Job</label>
                         <input type="text" className="form-control" id="exampleInputJob" 
                             name="job"
-                            value={val.job}
-                            onChange={setData}
+                            value={job}
+                            onChange={(e) => setJob(e.target.value)}
                         />
                     </div>
                     <div className="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputContact" className="form-label">Phone Number</label>
                         <input type="number" className="form-control" id="exampleInputContact" 
                             name="phone"
-                            value={val.phone}
-                            onChange={setData}
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
                         />
                     </div>
                     <div className="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputAddress" className="form-label">Address</label>
                         <input type="text" className="form-control"
                             name="address"
-                            value={val.address}
-                            onChange={setData}
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
                         />
                     </div>
                     <button type="submit" onClick={addData} className="btn btn-primary">Add</button>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,72 +1,86 @@
-import React from "react";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import {NavLink} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { NavLink } from "react-router-dom";
 
 function Home() {
-    return (
-        <div className="mt-5">
-            <div className="container">
-                <div className="add_btn mt-2">
-                    <NavLink to="/add" className="btn btn-primary">Add Data</NavLink>
-                </div>
 
-                <table className="table mt-2">
-                    <thead>
-                        <tr className="table-dark">
-                            <th scope="col">id</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Job</th>
-                            <th scope="col">Number</th>
-                            <th scope="col">Address</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Karishma</td>
-                            <td>karishma@gmail.com</td>
-                            <td>Front-end developer</td>
-                            <td>+923144678123</td>
-                            <td>Defence Phase-2</td>
-                            <td className="d-flex justify-content-between">
-                                <button className="btn btn-success"><VisibilityIcon /></button>
-                                <button className="btn btn-primary"><EditIcon /></button>
-                                <button className="btn btn-danger"><DeleteIcon /></button>
-                            </td>
-                        </tr>
-                        {/* <tr>
-                            <th scope="row">2</th>
-                            <td>Sadia</td>
-                            <td>sadia@gmail.com</td>
-                            <td>MERN Stack Developer</td>
-                            <td>+923223123456</td>
-                            <td>
-                                <button>Update</button>
-                                <button>View</button>
-                                <button>Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Zoya</td>
-                            <td>zoya@gmail.com</td>
-                            <td>Backend developer</td>
-                            <td>+923223236754</td>
-                            <td>
-                                <button>Update</button>
-                                <button>View</button>
-                                <button>Delete</button>
-                            </td>
-                        </tr> */}
-                    </tbody>
-                </table>
-            </div>
+  const [getuserdata, setuserdata] = useState([]);
+  console.log(getuserdata);
+
+  const getData = async (e) => {
+    const res = await fetch("/getdata", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    if (res.status === 404 || !data) {
+      console.log("Error");
+    } else {
+      setuserdata(data);
+      console.log("Get Data!");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="mt-5">
+      <div className="container">
+        <div className="add_btn mt-2">
+          <NavLink to="/add" className="btn btn-primary">
+            Add Data
+          </NavLink>
         </div>
-    )
+        <table className="table mt-2">
+          <thead>
+            <tr className="table-dark">
+              <th scope="col">id</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Job</th>
+              <th scope="col">Number</th>
+              <th scope="col">Address</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {getuserdata.map((val, key) => {
+              return (
+                <>
+                  <tr>
+                    <th scope="row">{key + 1}</th>
+                    <td>{val.name}</td>
+                    <td>{val.email}</td>
+                    <td>{val.job}</td>
+                    <td>{val.phone}</td>
+                    <td>{val.address}</td>
+                    <td className="d-flex justify-content-between">
+                      <button className="btn btn-success">
+                        <VisibilityIcon />
+                      </button>
+                      <button className="btn btn-primary">
+                        <EditIcon />
+                      </button>
+                      <button className="btn btn-danger">
+                        <DeleteIcon />
+                      </button>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
